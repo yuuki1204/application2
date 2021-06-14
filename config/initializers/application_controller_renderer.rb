c9d151993b65d1933d367fb40d78980fa39bd227
yuuki1204 <vocalistyuuki@gmail.com>
2021-06-14 14:30:42 +0000

    2ndcommit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '5b0bb75b883db23e16751120714e9694777aa172a260615fb0942f15b8c0f1134dcf64ba5a5a402d750d106e64d6bb9043e2ef76708aa48d998e97ecb46e5130'
